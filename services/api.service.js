823c13b94f2740125d4ad7380908ea98e165d9bd
enterdone <fm0258@gmail.com>
2023-02-19 17:19:24 +0400

    19/02

--- services/api.service.js
+++ services/api.service.js
@@ -1,32 +1,94 @@
 import https from "https";
-import { getKeyValue } from "./storage.service";
+import { getKeyValue } from "./storage.service.js";
 
 
 const getWeather = async (city) => {
 	const token = await getKeyValue("Token")
-	if(!token){
+	if (!token) {
 		throw new Error("not have Token. Sry :( \n try get it for my by command -t [API KEY]")
 	}
-// const url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-const url = new URL("https://api.openweathermap.org/data/2.5/weather")
-url.searchParams.append('q', city);
-url.searchParams.append('appis', city);
-url.searchParams.append('lang', 'ru');
-url.searchParams.append('units' , 'metric');
+
+	fetch()
 
 
-https.get(url,(response)=>{
-	let res = '';
-	response.on('data', (chunk)=>{
-		res+= chunk;
-	})  
 
-response.on('end', ()=>{
-	console.log(res)
-})
+	const isLena = (text) => {
+		const result = {};
+		const objects = [];
+		text = ();text.replace(/[^a-zA-Z]/g, "").toLowerCase
+			 
+			 if (!result[item]) {
+				  result[item] = 1;
+			 } else {
+			 result[item] += 1;
+			 }
+		}
+		 const entries = Object.entries(result);
+		 for (const [key, value] of entries) {
+			  objects.push({letter: key, count: value})
+		 } 
+		 
+		 
+		 
+  
+		return(objects.sort((a, b) => Math.sign(a.count - b.count)));
+  }
+  
 
-// response.on("error", (error)=>{ console.log("WTF??"+error)})
-})
+
+
+
+
+
+
+///////////////////////////////////
+/////////////ПРИМЕР НА FETCH
+
+// /*это в фунцию getweather*/const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&lang=ru&units=metric`;
+// //ИЛИ
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + token + "&lang=ru&units=metric";
+
+// console.log(apiUrl);
+
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//     // здесь можно обрабатывать полученные данные
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
+
+
+
+///////////////////////////////////
+/////////////ПРИМЕР НА NEW URL
+// 	// const url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+// 	const url = new URL("https://api.openweathermap.org/data/2.5/weather")
+// 	url.searchParams.append('q', city);
+// 	url.searchParams.append('appid', token);
+// 	url.searchParams.append('lang', 'ru');
+// 	url.searchParams.append('units', 'metric');
+
+// console.log(url)
+// 	https.get(url, (response) => {
+// 		let res = '';
+// 		response.on('data', (chunk) => {
+// 			res += chunk;
+// 		})
+
+// 		response.on('end', () => {
+// 			console.log(res)
+// 		})
+
+// 		response.on("error", (error)=>{ console.log("WTF??"+error)})
+// 	})
 }
 
-export {getWeather}
+export { getWeather }
